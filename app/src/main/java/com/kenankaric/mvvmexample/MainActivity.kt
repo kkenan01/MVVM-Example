98f70fec320267f04735cdeae3eaa6a6c4877529
kkenan01 <kenan.karic@propeller.ba>
2022-12-14 22:18:57 +0100

    Initial commit

---
+++ app/src/main/java/com/kenankaric/mvvmexample/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.kenankaric.mvvmexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
+import com.kenankaric.mvvmexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Using [ActivityMainBinding] we're providing safe access to our UI components
+     * by view binding.
+     */
+    private lateinit var binding: ActivityMainBinding
+
+    /**
+     * We are lazily injecting our [MainViewModel].
+     * Meaning, it'll only be called when it's actually needed, so it doesn't waste resources.
+     */
+    private val viewModel: MainViewModel by viewModels()
+
+    companion object {
+        /**
+         * This is for consistency. It clearly indicates that our message on the UI will either
+         * contain name or not. It makes code easier to read.
+         */
+        private const val EMPTY_NAME = ""
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /**
+         * Here we're inflating our layout.
+         * [binding] magically knows which layout belongs to which activity/fragment.
+         */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupUI()
+        updateGreetingMessage()
+    }
+
+    private fun setupUI() {
+        /**
+         * We set initial user message to be rendered on the screen.
+         * We do this because our string [greeting_message] contains placeholder for the username.
+         *
+         * Therefore we need to provide format arguments to [getString] method
+         * (we provide a const value [EMPTY_NAME]).
+         *
+         * We could use empty string directly, like so >> getString(R.string.greeting_message, "").
+         * However this is for both consistency and clarity sake. So we're either passing
+         * name or not to the string as a format argument.
+         */
+        binding.lblGreetingMessage.text = getString(R.string.greeting_message, EMPTY_NAME)
+
+        /**
+         * We collect value from the [MainViewModel], which we made to be **read-only** observable.
+         * This is important because we want data to flow in one direction, ie. from [MainViewModel] to UI.
+         *
+         * We then update our [greeting_message] with format argument of our newly written name.
+         *
+         * We say that our lifecycle owner is [MainActivity]. Once that is destroyed,
+         * we'll stop observing the data.
+         */
+        viewModel.name.observe(this@MainActivity) { name ->
+            binding.lblGreetingMessage.text = getString(R.string.greeting_message, name)
+        }
+    }
+
+    /**
+     * Here we're listening to any change that is happening to our input field,
+     * making it send data to the [MainViewModel] to update the state,
+     * which we're observing/listening above.
+     */
+    private fun updateGreetingMessage() {
+        binding.etUsername.addTextChangedListener { text: Editable? ->
+            viewModel.updateName(text.toString())
+        }
+    }
+}
---
+++ app/src/main/java/com/kenankaric/mvvmexample/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.kenankaric.mvvmexample
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    /**
+     * Using mutable live data we're (as the name state) mutating the state (duh!)
+     * which in our case is of type [String].
+     *
+     * It's important to keep such variables private since we don't want variables that mutate state
+     * to leave the scope of the view model, or in any way leak into the UI.
+     */
+    private val _name: MutableLiveData<String> = MutableLiveData("")
+
+    /**
+     * We are exposing to the UI a **read only** [LiveData] which will contain our value
+     * which we get by grabbing it from our mutating variable above.
+     */
+    val name: LiveData<String> = _name
+
+    /**
+     * In order to mutate the state (which will be observed in the UI),
+     * we're exposing public method which has access to our mutable live data.
+     *
+     * This is okay, since user/UI can only access/change the value here, ie. still not directly.
+     *
+     * Value is dispatched on the main thread.
+     */
+    fun updateName(username: String) {
+        _name.postValue(username)
+    }
+}
